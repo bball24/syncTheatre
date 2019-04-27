8b63facbaefa514c29baf3fac054bfc0a8067b74
BrettKorp <brettkorp@gmail.com>
2019-04-27 15:35:57 -0700

    removed node modules + testing

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.json({
+		name: 'cool express app',
+		author: 'syncTheatre'
+	});
+	//res.send('This is my cool homepage!!! CS180!!');
+});
+
+router.get('/square/:num1', function(req, res){
+	var a = req.params.num1;
+	console.log(a);
+	var squared = a * a;
+	res.json({ result : squared})
 });
 
 module.exports = router;
