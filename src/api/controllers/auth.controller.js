e3130265754c1688ad8b4f06e7f48851f180d2cd
Daniel D'Orange <danieldorange1@gmail.com>
2019-05-19 18:57:17 -0700

    Finished redirect route for use with landing page and modified passport setup to append token to redirect request.

--- src/api/controllers/auth.controller.js
+++ src/api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 // Created by Dan
 let express = require('express');
 let router = express.Router();
+let UserModel = require('../models/user.model');
 const passport = require('passport');
 
 router.get('/google', passport.authenticate('google', {
@@ -8,9 +9,19 @@ router.get('/google', passport.authenticate('google', {
 }));
 
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-    console.log(req.user);
-    res.status(200).json(req.user);
-    res.send("this is the callback URL");
+    if(!req.user)
+    {
+        res.redirect('http://localhost:3000/token/?s=false')
+    }
+
+    const user = new UserModel(false);
+    user.registerUser(req.user.displayName, req.user.id, req.user.provider);
+    user.createRegisteredUser().then((doc) => {
+        res.redirect('http://localhost:3000/token/?tok=' + req.user.token + '$s=true&uid=' + user.oauthID )
+    })
+    .catch((err) => {
+            res.redirect('http://localhost:3000/token/?s=false')
+    })
 });
 
 module.exports = router;
