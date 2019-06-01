2fed5e624733d2b3221c1f821848c19cbb92bbdb
bball24 <jerry88tan@gmail.com>
2019-06-01 15:07:48 -0700

    getting 404

--- src/frontend/src/components/Profile.js
+++ src/frontend/src/components/Profile.js
@@ -1,22 +1,28 @@
 import React, { Component } from "react";
 import "./Home.scss";
-import UserToken from './UserToken'
-import queryString from "query-string";
+import axios from 'axios';
+import queryString from 'query-string';
+import Button from 'react-bootstrap/Button';
 
 export default class Profile extends Component {
     constructor(props) {
         super(props);
         const values = queryString.parse(this.props.location.search);
         this.state = {
-            token : values.tok || "",
-            successful : (values.s == 'true'),
             userID : values.uid,
             apiHost : this.props.apiHost,
-            user : {},
             roomName : "",
+            photo : "",
             userName : "",
             badName : false
         };
+        axios.get(this.state.apiHost + '/api/users/ ' + this.state.userID)
+            .then((user) => {
+                console.log(user.data.userName);
+            })
+            .catch((err)=>{
+            console.error(err);
+        })
 
     }
     render() {
@@ -24,9 +30,10 @@ export default class Profile extends Component {
             <div className="outerWrap">
                 <div className="innerWrap">
                     <h2>Profile</h2>
-                    <p className="profile">
-                        {this.state.userName}
-                    </p>
+                    <span className="profile">
+                            Current UserName: {this.state.userName}
+                        <Button variant="primary" onClick={this.handleSubmit}>Change UserName</Button>
+                    </span>
                 </div>
             </div>
         );
